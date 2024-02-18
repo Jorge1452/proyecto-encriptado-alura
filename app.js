5f8915ab7e3e9eb89396771a51ed0a3370a8c170
Jorge Cruz <cruzerojorge@gmail.com>
2024-02-17 18:55:04 -0700

    Adiccion de la funcionalidad para desencriptar un texto

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 function encriptar() {
     let texto = document.getElementById("texto").value;
-    //let tituloMensaje = document.getElementById("titulo-mensaje");
-    //let parrafo = document.getElementById("parrafo");
-    //let muñeco = document.getElementById("muñeco");
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
 
     let textoCifrado = texto
                         .replace(/e/gi, "enter")
@@ -11,13 +11,43 @@ function encriptar() {
                         .replace(/o/gi, "ober")
                         .replace(/u/gi, "ufat");
                         
-    if (document.getElementById("texto").value.legth != 0  ) {
+    if (texto.legth != 0 ) {
         document.getElementById("texto").value = textoCifrado;
-        document.getElementById("titulo-mensaje") = "texto encriptado con éxito";
-        document.getElementById("parrafo").textContent = "";
-        document.getElementById("muñeco").src = "./img/muñeco.png";
+        tituloMensaje.textContent = "texto encriptado con éxito";
+        parrafo.textContent = "";
+        muñeco.src = "./img/muñeco.png";
     } else {
-        document.getElementById("muñeco").src = "./img/muñeco.jpg";
+        muñeco.src = "./img/muñeco.jpg";
+        tituloMensaje.textContent = "Ningún mensaje fue encotrado";
+        parrafo.textContent = "Ingresa el texto";
         alert("Debes ingresar algún texto");
     }
+}
+
+function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+
+    let textoCifrado = texto 
+                        .replace(/enter/gi, "e")
+                        .replace(/imes/gi, "i")
+                        .replace(/ai/gi, "a")
+                        .replace(/ober/gi, "o")
+                        .replace(/ufat/gi, "u");
+    
+    if (texto.legth != 0 ) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "texto desencriptado con éxito";
+        parrafo.textContent = "";
+        muñeco.src = "./img/muñeco.jpg";
+    } else {
+        muñeco.src = "./img/muñeco.jpg";
+        tituloMensaje.textContent = "Ningún mensaje fue encotrado";
+        parrafo.textContent = "Ingresa el texto";
+        alert("Debes ingresar algún texto");
+    }
+    
+
 }
